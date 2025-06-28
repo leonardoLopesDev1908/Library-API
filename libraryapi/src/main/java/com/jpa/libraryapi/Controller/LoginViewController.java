6654ad47fa197ecfe43dbbc5c33d850390380a0a
Leonardo Lopes <leonardosilvalls1908@gmail.com>
2025-06-28 16:35:09 -0300

    gitignore

--- libraryapi/src/main/java/com/jpa/libraryapi/Controller/LoginViewController.java
+++ libraryapi/src/main/java/com/jpa/libraryapi/Controller/LoginViewController.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.jpa.libraryapi.security.CustomAuthentication;
+
 @Controller
 public class LoginViewController {
     
@@ -16,6 +18,10 @@ public class LoginViewController {
     @GetMapping("/")
     @ResponseBody
     public String paginaHome(Authentication authentication){
+        if (authentication instanceof CustomAuthentication customAuth){
+            System.out.println(customAuth.getUsuario());
+        }
+        
         return "Olá " + authentication.getName();
     }
 }
