7287407888008561ba7b48a0bc557fc5f203763c
Leonardo Lopes <leonardosilvalls1908@gmail.com>
2026-09-21 10:32:44 -0300

    security: form login adicionado

--- src/main/java/com/jpa/libraryapi/config/SecurityConfig.java
+++ src/main/java/com/jpa/libraryapi/config/SecurityConfig.java
@@ -1,38 +1,18 @@
 package com.jpa.libraryapi.config;
 
-import com.jpa.libraryapi.common.ApiConstants;
-import com.jpa.libraryapi.user.AuthorUserDetails;
 import com.jpa.libraryapi.user.AuthorUserDetailsService;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Configurable;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
-import org.springframework.security.config.annotation.authorization.EnableMultiFactorAuthentication;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.FactorGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.crypto.password4j.BcryptPassword4jPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Slf4j
 @Configuration
@@ -47,14 +27,16 @@ public class SecurityConfig {
         return http
                 .csrf(csrf -> csrf.disable())
                 .cors(Customizer.withDefaults())
+                .authenticationProvider(authProvider())
                 .authorizeHttpRequests((requests) -> requests
                         .requestMatchers("/api/book").permitAll()
                         .requestMatchers(HttpMethod.POST, "/api/author").permitAll()
-                        .requestMatchers("/api/author/**").authenticated()
-                        .anyRequest().permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .anyRequest().authenticated()
                 )
-                .addFilterBefore(RateLimitingFilter.class.newInstance(), UsernamePasswordAuthenticationFilter.class)
+                //.addFilterBefore(RateLimitingFilter.class.newInstance(), UsernamePasswordAuthenticationFilter.class)
                 .httpBasic(Customizer.withDefaults())
+                .formLogin(Customizer.withDefaults())
                 .logout(LogoutConfigurer::permitAll)
                 .build();
     }
